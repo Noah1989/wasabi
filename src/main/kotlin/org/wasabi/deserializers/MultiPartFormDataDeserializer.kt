fd4b68255bc016328708b006b4be9ad7e75d3d96
Stefan Noack <stefan@noack-software.de>
2015-08-07 20:02:11 +0200

    rudimentary file upload support

--- src/main/kotlin/org/wasabi/deserializers/MultiPartFormDataDeserializer.kt
+++ src/main/kotlin/org/wasabi/deserializers/MultiPartFormDataDeserializer.kt
@@ -1,11 +1,12 @@
 package org.wasabi.deserializers
 
+import io.netty.handler.codec.http.multipart.Attribute
+import io.netty.handler.codec.http.multipart.FileUpload
 import io.netty.handler.codec.http.multipart.InterfaceHttpData
 import io.netty.handler.codec.http.multipart.InterfaceHttpData.HttpDataType
-import io.netty.handler.codec.http.multipart.Attribute
 import java.util.HashMap
 
-public class MultiPartFormDataDeserializer: Deserializer("application/x-www-form-urlencoded", "multipart/form-data") {
+public class MultiPartFormDataDeserializer : Deserializer("application/x-www-form-urlencoded", "multipart/form-data(;.*)?") {
     val bodyParams = HashMap<String, Any>()
 
     override fun deserialize(input: Any): HashMap<String, Any> {
@@ -14,19 +15,24 @@ public class MultiPartFormDataDeserializer: Deserializer("application/x-www-form
     }
 
     private fun parseBodyParams(httpDataList: List<InterfaceHttpData>) {
-        for(entry in httpDataList) {
+        for (entry in httpDataList) {
             addBodyParam(entry)
         }
     }
 
     private fun addBodyParam(httpData: InterfaceHttpData) {
         // TODO: Add support for other types of attributes (namely file)
-        if (httpData.getHttpDataType() == HttpDataType.Attribute) {
-            val attribute = httpData as Attribute
-            bodyParams[attribute.getName().toString()] = attribute.getValue().toString()
+        when (httpData.getHttpDataType()) {
+            HttpDataType.Attribute -> {
+                val attribute = httpData as Attribute
+                bodyParams[attribute.getName().toString()] = attribute.getValue().toString()
+            }
+            HttpDataType.FileUpload -> {
+                val upload = httpData as FileUpload
+                bodyParams[upload.getName().toString()] = upload.getString().toString()
+            }
         }
     }
-
 
 
 }
